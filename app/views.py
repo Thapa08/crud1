003593ae940bd1c99b8a6e351a5da97da96d1940
Thapa08 <swarupthapa955@gmail.com>
2023-05-11 16:00:23 +0545

    1st commit

---
+++ app/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+# Create your views here.
+
+def home(request):
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render())
+
+def about(request):
+    template = loader.get_template('about.html')
+    return HttpResponse(template.render())
